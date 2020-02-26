c3bd8e10298f3bfc5d3697cbeba97f5ff714cb2c
Nikki Vaughan <vnikki13@gmail.com>
2020-02-25 16:37:30 -0800

    add average_rating and total_revenue methods

--- lib/driver.rb
+++ lib/driver.rb
@@ -24,6 +24,26 @@ module RideShare
       @trips << trip
     end
 
+    def average_rating
+      return 0 if trips.length == 0
+      trip_ratings = trips.map do |trip|
+        trip.rating.to_f
+      end
+
+      return trip_ratings.sum / trip_ratings.length
+    end
+
+    def total_revenue
+      trip_revenues = trips.map do |trip|
+        if trip.cost > 1.65
+          (trip.cost - 1.65) * 0.8
+        else
+          0
+        end
+      end
+      return trip_revenues.sum.round(2)
+    end
+
     private
 
     def self.from_csv(record)
