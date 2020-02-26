2533a5ee9d8af2b23bf09b4aecc545c24b039bf4
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-02-25 21:32:45 -0800

    Added test for Passenger total_time_spent method

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -43,9 +43,5 @@ module RideShare
         phone_number: record[:phone_num]
       )
     end
-
-    # Wave 1: Add an instance method, net_expenditures, to Passenger that will return the total amount of money that passenger has spent on their trips
-    # Wave 1: Add an instance method, total_time_spent to Passenger that will return the total amount of time that passenger has spent on their trips
-    # Each of these methods must have tests. What happens if the passenger has no trips?
   end
 end
--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require 'time'
 
 describe "Passenger class" do
 
@@ -71,21 +72,34 @@ describe "Passenger class" do
   # Wave 1: tests for the net_expenditures method
   describe "net_expenditures" do
     it "returns correct total amount spent" do
-      # set up data
       test_trips = []
-      test_trips << RideShare::Trip.new(id: 395, passenger: nil, passenger_id: 1, start_time: '2018-10-31 01:02:22 -0700', end_time: '2018-10-31 01:48:15 -0700', cost: 6, rating: 2)
-      test_trips << RideShare::Trip.new(id: 441, passenger: nil, passenger_id: 1, start_time: '2018-12-27 01:57:48 -0800', end_time: '2018-12-27 02:42:05 -0800', cost: 9, rating: 1)
+      test_trips << RideShare::Trip.new(id: 395, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-10-31 01:02:22 -0700'), end_time: Time.parse('2018-10-31 01:48:15 -0700'), cost: 6, rating: 2)
+      test_trips << RideShare::Trip.new(id: 441, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-12-27 01:57:48 -0800'), end_time: Time.parse('2018-12-27 02:42:05 -0800'), cost: 9, rating: 1)
       passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: test_trips)
       expect(passenger.net_expenditures).must_equal 15
     end
 
     it "shows when $0 spent" do
-      # set up data
       test_trips = []
-      test_trips << RideShare::Trip.new(id: 395, passenger: nil, passenger_id: 1, start_time: '2018-10-31 01:02:22 -0700', end_time: '2018-10-31 01:48:15 -0700', cost: 0, rating: 2)
-      test_trips << RideShare::Trip.new(id: 441, passenger: nil, passenger_id: 1, start_time: '2018-12-27 01:57:48 -0800', end_time: '2018-12-27 02:42:05 -0800', cost: 0, rating: 1)
+      test_trips << RideShare::Trip.new(id: 395, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-10-31 01:02:22 -0700'), end_time: Time.parse('2018-10-31 01:48:15 -0700'), cost: 0, rating: 2)
+      test_trips << RideShare::Trip.new(id: 441, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-12-27 01:57:48 -0800'), end_time: Time.parse('2018-12-27 02:42:05 -0800'), cost: 0, rating: 1)
       passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: test_trips)
       expect(passenger.net_expenditures).must_equal 0
+    end
+  end
+
+  # Wave 1: tests for the total_time_spent method
+  describe "total_time_spent" do
+    it "returns correct total time spent" do
+      test_trips = []
+      test_trips << RideShare::Trip.new(id: 395, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-10-31 01:02:22 -0700'), end_time: Time.parse('2018-10-31 01:48:15 -0700'), cost: 6, rating: 2)
+      test_trips << RideShare::Trip.new(id: 441, passenger: nil, passenger_id: 1, start_time: Time.parse('2018-12-27 01:57:48 -0800'), end_time: Time.parse('2018-12-27 02:42:05 -0800'), cost: 9, rating: 1)
+      passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: test_trips)
+      expected_time = 0
+      test_trips.each do |trip|
+        expected_time += trip.calculate_duration
+      end
+      expect(passenger.total_time_spent).must_equal expected_time
     end
   end
 
