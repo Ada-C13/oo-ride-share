9e0c9c6adb9c2313585970d5316488e6ea0cec74
Jessica Stone <jstone4815@gmail.com>
2020-02-26 13:53:08 -0800

    updated average_rating and total_revenue methods to account for nil values

--- lib/driver.rb
+++ lib/driver.rb
@@ -27,22 +27,17 @@ module RideShare
     end
 
     def average_rating
-      return 0 if trips.length == 0
-      trip_ratings = trips.map do |trip|
-        trip.rating.to_f
-      end
+      completed_trips = trips.select{ |trip| trip.rating != nil }
 
+      trip_ratings = completed_trips.map { |trip| trip.rating.to_f }
+
+      return 0 if trip_ratings.length == 0
       return trip_ratings.sum / trip_ratings.length
     end
 
     def total_revenue
-      trip_revenues = trips.map do |trip|
-        if trip.cost > 1.65
-          (trip.cost - 1.65) * 0.8
-        else
-          0
-        end
-      end
+      completed_trips = trips.select{ |trip| trip.cost != nil && trip.cost > 1.65 }
+      trip_revenues = completed_trips.map { |trip| (trip.cost - 1.65) * 0.8 }
       return trip_revenues.sum.round(2)
     end
 
