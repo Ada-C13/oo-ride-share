cf520516f84926b993bce6f6afd70ad2ab170df3
Jessica Stone <jstone4815@gmail.com>
2020-02-26 15:34:29 -0800

    added pick_driver_for_trip helper method and added new logic for wave 4

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -33,29 +33,38 @@ module RideShare
               #{passengers.count} passengers>"
     end
 
-    def request_trip(passenger_id)
-      # TODO: finish writing method and make it work
-      available_driver = ""
+    def pick_driver_for_trip
+      available_drivers = @drivers.select{ |driver| driver.status == :AVAILABLE}
+      raise "No drivers are currently available" if available_drivers.length == 0
 
-      # get the first available driver id
-      @drivers.each do |driver|
-        if driver.status == :AVAILABLE
-          available_driver = driver
-          driver.status = :UNAVAILABLE
-          break
+      picked_driver = ""
+      oldest_end_time = Time.now
+
+      available_drivers.each do |driver|
+        if driver.trips.length == 0
+          picked_driver = driver
+          return picked_driver
+        else
+          driver_most_recent_end_time = driver.trips.max_by { |trip| trip.end_time }.end_time
+          if driver_most_recent_end_time < oldest_end_time
+            oldest_end_time = driver_most_recent_end_time
+            picked_driver = driver
+          end
         end
       end
 
-      raise "No drivers are currently available" if available_driver == ""
+      return picked_driver
+    end
+
+    def request_trip(passenger_id)
 
       found_passenger = find_passenger(passenger_id)
-
+      available_driver = pick_driver_for_trip
       new_trip = Trip.new(id: @trips.length + 1, passenger: found_passenger, start_time: Time.now, end_time: nil, cost: nil, rating: nil, driver_id: available_driver.id, driver: available_driver)
 
-      available_driver.trips.push(new_trip)
-      found_passenger.trips.push(new_trip)
+      available_driver.trip_in_progress(new_trip)
+      found_passenger.add_trip(new_trip)
       @trips.push(new_trip)
-
 
       return new_trip
     end
