bba520cfd6a19256b3e4ee1a5fb5e69b6967a20a
ChelseaC13 <chelhelg@outlook.com>
2020-02-25 18:15:05 -0800

    Stuck in the middle of wave 2

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,6 +1,8 @@
 require 'csv'
+require 'awesome_print'
 
 require_relative 'csv_record'
+require 'pry'
 
 module RideShare
   class Driver < CsvRecord
@@ -8,19 +10,47 @@ module RideShare
 
     def initialize(id:, name:, vin:, status:, trips: nil)
       super(id)
+      
 
-        @id = id
-        @name = name
-        @vin = vin
-        @status = status
-        @trips =trips || []
+      @name = name
+      @vin = vin
+      @status = status
+      @trips = trips || []
+
+      approved_status = [:AVAILABLE, :UNAVAILABLE]
+
+      unless @vin.length != 17 || @vin.is_a?String
+        raise ArgumentError.new ('Your VIN is wrong.')
+      end
+
+      unless approved_status.include?(@status)
+        raise ArgumentError.new ('You must provide one of the following statuses :available, :unavailable')
+      end
+
+    
 
 
     end
 
+    def add_trip(trip)
+      @trips << trip
+    end
 
+    private   
 
-
+    def self.from_csv(record)
+      return new(
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status]
+      )
+    end
 
   end
+
+# drivers = RideShare::Driver.load_all(full_path: '../support/drivers.csv')
+# ap drivers
 end
+
+
--- lib/trip.rb
+++ lib/trip.rb
@@ -4,7 +4,7 @@ require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :driver_id, :driver
 
     def initialize(
           id:,
@@ -13,20 +13,30 @@ module RideShare
           start_time:,
           end_time:,
           cost: nil,
-          rating:
+          rating:,
+          driver_id: nil,
+          driver: nil
         )
       super(id)
 
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
-
       elsif passenger_id
         @passenger_id = passenger_id
-
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
       end
+
+
+      # if driver
+      #   @driver = driver
+      #   @driver_id = driver.id
+      # elsif driver_id
+      #   @driver_id = driver_id
+      # else
+      #   raise ArgumentError, 'Driver or driver_id is required'
+      # end
 
       if start_time.class == String
         start_time = Time.parse(start_time) # added Time.parse to turn string into time, if it was passed as a string.
