0dcfd670bdc1ef2396182a779176be3145cf6f45
Stephanie <stephaniejmars@gmail.com>
2020-02-25 18:25:21 -0800

    added net expenditures

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -17,6 +17,15 @@ module RideShare
       @trips << trip
     end
 
+    def net_expenditures #(passenger id?)
+      # result = @trips.cost.sum
+      return @trips.map {|trip| trip.cost}.sum
+      #sum costs  #total amount of money pax as spent 
+
+      # return @trips.map {|trip| trip.cost}.sum
+
+    end
+
     private
 
     def self.from_csv(record)
@@ -26,5 +35,13 @@ module RideShare
         phone_number: record[:phone_num]
       )
     end
+
   end
 end
+
+# # Add an instance method, net_expenditures, to Passenger that will return the total amount of money that passenger has spent on their trips
+# def net_expenditures#(passenger id?)
+#   restult = @trips.cost.sum
+#   return result
+#   #sum costs  #total amount of money pax as spent 
+# end
