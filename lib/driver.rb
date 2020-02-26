cb9bee7011c20d32e5ff7b59b08d727bb31b80e6
Alicia Combs <alicia.combs.92@gmail.com>
2020-02-26 14:24:32 -0800

    Added total_revenue method to the driver class, wrote tests for it and passed them

--- lib/driver.rb
+++ lib/driver.rb
@@ -29,13 +29,30 @@ module RideShare
       if trips.length == 0
         return 0
       end
-      
+
       total_rating = 0.0
 
       trips.each do |trip|
         total_rating += trip.rating
       end
       return total_rating / trips.length
+    end
+
+    def total_revenue
+      if trips.length == 0
+        return 0
+      end
+
+      total = 0.0
+
+      trips.each do |trip|
+        if trip.cost > 1.65
+          total += (trip.cost - 1.65)
+        else
+          total += trip.cost
+        end
+      end
+      return (total * 0.8).round(2)
     end
 
     private
