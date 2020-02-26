e816187fbbf56a45c148645fbf339c95e52125e6
Leah Hughes <leahwhughes@gmail.com>
2020-02-26 14:30:25 -0800

    add status check

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,25 +3,57 @@ require_relative 'csv_record'
 module RideShare
   class Driver < CsvRecord
     attr_reader :name, :vin, :status, :trips
-
+    
     def initialize(id:, name:, vin:, status: :AVAILABLE, trips: nil)
       super(id)
-
+      
       @name = name
       @vin = vin
-        if @vin.length != 17
-          raise ArgumentError, "vin length must be 17 characters"
-        end
-      @status = status
-      # if status does not equal :AVALIABLE || :UNAVAILABLE
-      # raise ArgumentError, "status must be :available or un...
-      # end"
-      if !([:AVAILABLE, :UNAVAILABLE].include? @status) 
-        raise ArgumentError, "status must be :AVAILABLE or :UNAVAILABLE"
+      if @vin.length != 17
+        raise ArgumentError, "vin length must be 17 characters"
+      end
+      @status = status.to_sym
+      valid_status = [:AVAILABLE, :UNAVAILABLE]
+
+      if !(valid_status.include? status.to_sym)
+        raise ArgumentError, "invalid status"
       end
       
       @trips = trips || []
+      
     end
-  end
+    
+    def add_trips(trip)
+      @trips << trip
+    end
+    
+    private
+    
+    def self.from_csv(record)
+      return Driver.new( 
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status]
+      )
+    end
+    
+  end # end Driver Class
   
-end
+end # end Rideshare
+
+# drivers = RideShare::Driver.load_all(full_path: '../support/drivers.csv')
+# ap drivers
+
+
+# if status does not equal :AVALIABLE || :UNAVAILABLE
+# raise ArgumentError, "status must be :available or un...
+# end"
+# if !([:AVAILABLE, :UNAVAILABLE].include? @status) 
+#   raise ArgumentError, "status must be :AVAILABLE or :UNAVAILABLE"
+# end
+
+
+# if status != :AVAILABLE || status != :UNAVAILABLE
+#   raise ArgumentError, "status must be :AVAILABLE or :UNAVAILABLE"
+# end
