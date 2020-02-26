c3310ba07dcfcbb18bcd19caf50d73905be553f5
Vera <lizeth.veraro@outlook.com>
2020-02-25 16:59:34 -0800

    Wave 3 (Request_trip method created)

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -33,6 +33,45 @@ module RideShare
               #{passengers.count} passengers>"
     end
 
+    def request_trip(passenger_id)
+      available_driver = nil
+
+      @trips.each do |trip|
+        if trip.driver.status == :AVAILABLE
+          available_driver = trip.driver.id
+          trip.driver.status = :UNAVAILABLE
+        end
+      end
+      # first_driver = @trips.select do |trip|
+      #   trip.driver.status == :AVAILABLE
+      # end
+      new_trip_id = @trips.length
+
+      new_trip = Trip.new(
+        id: new_trip_id,
+        passenger: find_passenger(passenger_id),
+        passenger_id: passenger_id,
+        start_time: Time.now(),
+        end_time:nil,
+        cost: nil,
+        rating:nil,
+        driver_id: available_driver,
+        driver: find_driver(available_driver)
+      )
+      Passenger.new(
+        id: passenger_id,
+        name: find_passenger(passenger_id).name,
+        phone_number:find_passenger(passenger_id).phone_num
+      )
+      passenger.add_trip(new_trip)
+
+      CSV.open("trips.csv","a") do |csv|
+        csv << [new_trip.id, new_trip.driver_id, new_trip.passenger_id, new_trip.start_time,
+          new_trip.end_time, new_trip.cost, new_trip.rating]
+      end
+      return new_trip
+    end
+
     private
 
     def connect_trips
