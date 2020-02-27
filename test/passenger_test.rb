0c497daf7d69045c395e7bc67d0b04a1a0287034
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-02-27 14:04:51 -0800

    added tests to passenger_test.rb

--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -74,45 +74,38 @@ describe "Passenger class" do
   describe "net_expenditures" do
     it "returns correct total amount spent" do
       all_trips = RideShare::Trip.load_all(directory: './support')
-      passenger_trips = []
-      passenger_cost = 0
 
-      passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: passenger_trips)
-
-      all_trips.each do |trip|
-        if trip.passenger_id == 1
-          passenger_cost += trip.cost
-          passenger_trips << trip
-        end
-      end
+      passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381')
+      passenger.add_trip(all_trips[0])
+      passenger.add_trip(all_trips[1])
       
-      expect(passenger.net_expenditures).must_equal 15
+      expect(passenger.net_expenditures).must_equal 28
     end
-
-  #   it "shows when $0 spent" do
-  #     test_trips = []
-  #     driver = RideShare::Driver.new(id: 1, name: 'Da Vinci', vin: 'RFWNJWGU3Y8SD2VP0')
-  #     test_trips << RideShare::Trip.new(id: 395, driver: driver, passenger_id: 1, start_time: Time.parse('2018-10-31 01:02:22 -0700'), end_time: Time.parse('2018-10-31 01:48:15 -0700'), cost: 0, rating: 2, status: :AVAILABLE, trips: test_trips, total_revenue: 0)
-  #     test_trips << RideShare::Trip.new(id: 441, driver: driver, passenger_id: 1, start_time: Time.parse('2018-12-27 01:57:48 -0800'), end_time: Time.parse('2018-12-27 02:42:05 -0800'), cost: 0, rating: 1, status: :AVAILABLE, trips: test_trips, total_revenue: 0)
-  #     passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: test_trips)
-  #     expect(passenger.net_expenditures).must_equal 0
-  #   end
   end
 
-  # # Wave 1: tests for the total_time_spent method
-  # describe "total_time_spent" do
-  #   it "returns correct total time spent" do
-  #     test_trips = []
-  #     driver = RideShare::Driver.new(id: 1, name: 'Da Vinci', vin: 'RFWNJWGU3Y8SD2VP0')
-  #     test_trips << RideShare::Trip.new(id: 395, driver: driver, passenger_id: 1, start_time: Time.parse('2018-10-31 01:02:22 -0700'), end_time: Time.parse('2018-10-31 01:48:15 -0700'), cost: 6, rating: 2, status: :AVAILABLE, trips: test_trips, total_revenue: 5)
-  #     test_trips << RideShare::Trip.new(id: 441, driver: driver, passenger_id: 1, start_time: Time.parse('2018-12-27 01:57:48 -0800'), end_time: Time.parse('2018-12-27 02:42:05 -0800'), cost: 9, rating: 1, status: :AVAILABLE, trips: test_trips, total_revenue: 5)
-  #     passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381', trips: test_trips)
-  #     expected_time = 0
-  #     test_trips.each do |trip|
-  #       expected_time += trip.calculate_duration
-  #     end
-  #     expect(passenger.total_time_spent).must_equal expected_time
-  #   end
-  # end
+  # Wave 1: tests for the total_time_spent method
+  describe "total_time_spent" do
+    it "returns correct total time spent" do
+      all_trips = RideShare::Trip.load_all(directory: './support')
+
+      passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381')
+      passenger.add_trip(all_trips[0])
+      passenger.add_trip(all_trips[1])
+      
+      expect(passenger.total_time_spent).must_equal 5523.0
+    end
+  end
+
+  describe "add_trip" do
+    it "returns adding a trip" do
+      all_trips = RideShare::Trip.load_all(directory: './support')
+
+      passenger = RideShare::Passenger.new(id: 1, name: 'Paul Pollich', phone_number: '(358) 263-9381')
+      passenger.add_trip(all_trips[0])
+      passenger.add_trip(all_trips[1])
+      
+      expect(passenger.trips.length).must_equal 2
+    end
+  end
 
 end
