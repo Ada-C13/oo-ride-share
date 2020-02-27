26cd77534f1840ffa6f3de932d1d2a83bb75d02f
saraleecodes <sara.lee.higgins@gmail.com>
2020-02-27 14:13:07 -0800

    Wave 3: dries up the total_revenue method

--- lib/driver.rb
+++ lib/driver.rb
@@ -32,19 +32,12 @@ module RideShare
 			@trips.each do |trip|
 				ratings << trip.rating unless trip.rating == nil
 			end
-      return @trips.length == 0 ? 0 : (ratings.sum / ratings.length).to_f.round(1)
-    end
+			return @trips.length == 0 ? 0 : (ratings.sum / ratings.length).to_f.round(1)
+	  end
 
     def total_revenue
-      if @trips.length == 0
-        return 0
-      else
-        revenue = []
-        @trips.each do |trip| 
-          revenue << ((trip.cost - 1.65)* 0.8) unless trip.cost == nil
-        end
-        return revenue.sum.round(2)
-      end
+      return 0 if @trips.length == 0
+      return @trips.sum { |trip| trip.cost.nil? ? 0 : ((trip.cost - 1.65)* 0.8) }.round(2)
     end
 
     private
