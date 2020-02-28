00bbef08a9131735832bab05d1ffb07f91d5b589
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-02-27 16:55:58 -0800

    fixed indentation and syntax in TripDispatcher

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -1,16 +1,16 @@
-require 'csv'
-require 'time'
+require "csv"
+require "time"
 # require 'pry'
 
-require_relative 'passenger'
-require_relative 'trip'
-require_relative 'driver'
+require_relative "passenger"
+require_relative "trip"
+require_relative "driver"
 
 module RideShare
   class TripDispatcher
     attr_reader :drivers, :passengers, :trips, :drivers
 
-    def initialize(directory: './support')
+    def initialize(directory: "./support")
       @passengers = Passenger.load_all(directory: directory)
       @trips = Trip.load_all(directory: directory)
       # Wave 2: load Drivers
@@ -34,43 +34,48 @@ module RideShare
               #{trips.count} trips, \
               #{drivers.count} drivers, \
               #{passengers.count} passengers>"
+    end
+  end
+
+  # Wave 3
+  # creates a trip and assigns an available driver
+  def request_trip(passenger_id)
+    @drivers.each do |driver|
+      if driver.status == :AVAILABLE
+        driver = dispatch_driver # make this loop end faster
       end
+      # return dispatch_driver
     end
 
-    # Wave 3
-    # creates a trip and assigns an available driver 
-    def request_trip(passenger_id)
-      if driver.each do |driver|
-        if driver.status == :AVAILABLE
-          return driver
-        end
-      end
+    dispatch_driver.status = :UNAVAILABLE
 
-      Trip.new(
-        id: driver.id,
-        passenger: @passenger_id, # function
-        passenger_id: @passenger_id,
-        start_time: Time.now,
-        end_time: nil,
-        cost: nil,
-        rating: nil,
-        driver: driver,
-        driver_id: driver.id
-      )
+    trip = Trip.new(
+      id: dispatch_driver.id,
+      passenger: find_passenger(passenger_id),
+      passenger_id: passenger_id,
+      start_time: Time.now,
+      end_time: nil,
+      cost: nil,
+      rating: nil,
+      driver: dispatch_driver,
+      driver_id: dispatch_driver.id,
+    )
+
+    dispatch_driver.add_trip(trip)
+    trip.passenger.add_trip(trip)
+    @trips << trip
+    return trip
+  end
+
+  private
+
+  def connect_trips
+    @trips.each do |trip|
+      passenger = find_passenger(trip.passenger_id)
+      driver = find_driver(trip.driver_id)
+      trip.connect(passenger)
+      trip.connect2(driver)
     end
-    
-
-    private
-
-    def connect_trips
-      @trips.each do |trip|
-        passenger = find_passenger(trip.passenger_id)
-        driver = find_driver(trip.driver_id)
-        trip.connect(passenger)
-        trip.connect2(driver)
-      end
-
-      return trips
-    end
+    return trips
   end
 end
